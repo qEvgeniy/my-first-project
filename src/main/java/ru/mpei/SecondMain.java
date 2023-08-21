bb0218bb6297b65c3b52c2547743ff83c6ceea9c
pEvgeniy <popevgeniyv@yandex.ru>
2023-08-21 11:55:07 +0300

    feat: new sout

--- src/main/java/ru/mpei/SecondMain.java
+++ src/main/java/ru/mpei/SecondMain.java
@@ -4,5 +4,6 @@ public class SecondMain {
     public static void main(String[] args) {
         System.out.println("new sout");
         System.out.println("new sout 2");
+        System.out.println("new sout 3");
     }
 }
